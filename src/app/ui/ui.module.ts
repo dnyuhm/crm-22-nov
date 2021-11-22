357e2f66359802cea44fea3a1c092ec589574bba
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-22 16:21:49 +0100

    Commit 6 : UiComponent html / scss

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { UiComponent } from './components/ui/ui.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent],
   imports: [CommonModule],
+  exports: [UiComponent],
 })
 export class UiModule {}
